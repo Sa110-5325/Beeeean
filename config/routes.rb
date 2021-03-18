8142d5c5e20e1d15741331fe61b2c84c8b433cad
Sa110-5325 <sa110.35325@gmail.com>
2021-03-18 13:37:10 +0000

    [add]検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -30,5 +30,6 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
   end
   resources :activities, only: [:index]
+  get 'searches/search', as: 'search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
